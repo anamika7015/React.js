cfad32fdd18f505b8abb3404cca050046d5902f6
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-16 16:15:20 +0530

    this is the first react js

---
+++ Introduction/src/App.jsx
@@ -0,0 +1,18 @@
+import profilePic from './assets/profle.jpg'
+
+function App() {
+
+  return (
+    <>
+    <div className="card">
+      <img src={profilePic} alt="Profile Picture" />
+      <h1>Anamika Pandey</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa blanditiis praesentium, officia nihil voluptatibus corporis dolorem ad animi fuga illum! Minus, accusamus magni asperiores nulla quis iste velit possimus quasi!</p>
+
+    </div>
+    </>
+
+  )
+}
+
+export default App
